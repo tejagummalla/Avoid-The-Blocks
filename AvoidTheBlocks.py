2ada1a6be29d62865597c1cf8906085f05a66a78
Teja Gummalla <gummalla.e@husky.neu.edu>
2018-09-04 00:18:38 -0400

    Commit the code with initial set us

---
+++ AvoidTheBlocks.py
@@ -0,0 +1,147 @@
+import pygame
+import time
+import random
+
+pygame.init()
+display_width = 800
+display_height = 600
+ticks_per_sec=60
+
+img_width = 86
+img_height = 110
+
+black=(0,0,0)
+white=(255,255,255)
+red = (255,0,0)
+green=(0,255,0)
+
+
+#Setting the display of the Game
+gameDisplay=pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Let us Race')
+
+#car for the race
+car_img=pygame.image.load('race toy.png')
+
+def car(x,y):
+    gameDisplay.blit(car_img,(x,y))
+
+
+
+#game clock
+clock=pygame.time.Clock()
+
+#obstacles
+def obstacles(ob_x,ob_y,ob_w,ob_h,color):
+    pygame.draw.rect(gameDisplay,color,[ob_x,ob_y,ob_w,ob_h])
+    
+
+
+#text rendering
+def text_objects(text,font):
+    textSurface = font.render(text, True, red)
+    return textSurface, textSurface.get_rect()
+
+
+#display the text of the game.(GAME OVER).
+def message_display(text):
+    largeText=pygame.font.Font('freesansbold.ttf',115)
+    textSurface,textRect = text_objects(text,largeText)
+    textRect.center = ((display_width/2),(display_height/2));
+
+    gameDisplay.blit(textSurface,textRect)
+    pygame.display.update()
+
+    time.sleep(2)
+
+    game_loop()
+
+def score(count):
+    font = pygame.font.SysFont(None,25)
+    text = font.render("Score: "+str(count),True,black)
+    gameDisplay.blit(text,(0,0))
+def game_over():
+    message_display('GAME OVER')
+
+def game_loop():
+    x = display_width*0.45
+    y = display_height*0.8
+    x_change=0
+    y_change=0
+    #Game Exit Condition
+    crashed= False
+
+    ob_startx = random.randrange(0,display_width)
+    ob_starty = -600
+    ob_speed = 7
+    ob_width = 100
+    ob_height = 100
+
+    speed=6
+    count = 0
+
+    while not crashed:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+                elif event.key == pygame.K_UP:
+                    y_change = -4
+                elif event.key == pygame.K_DOWN:
+                    y_change = 4
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change=0
+
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change=0
+
+        y=y+y_change
+        x=x+x_change
+
+        #Not letting the car exit the canvas
+        if y >= display_height - img_height:
+            y = display_height - img_height
+        if y < 0:
+            y=0
+        if x >= display_width - 90 or x < 0:
+            game_over()
+
+            
+        gameDisplay.fill(white)
+
+        car(x,y)
+        score(count)
+        
+        obstacles(ob_startx,ob_starty,ob_width,ob_height,green)
+        ob_starty += ob_speed
+
+        #crash condition for the objects
+        if x + img_width >= ob_startx and x <= ob_startx + ob_width and y <= ob_starty + ob_height and y >= ob_starty:
+            game_over()
+
+        if ob_starty > display_height:
+            ob_starty = 0 - ob_height
+            ob_startx = random.randrange(0,display_width)
+            ob_speed =  speed + (count/5) #random.randrange(4,7)
+            count=count+1
+        
+        
+        pygame.display.update()
+        clock.tick(ticks_per_sec)
+
+
+game_loop()
+
+pygame.quit()
+
+quit()
---
+++ README.md
@@ -0,0 +1,25 @@
+# Avoid The Blocks
+
+This is a Python based game made using the PyGame library. The objective of the 
+game is to avoid the falling blocks by using the arrow keys to move your race car. The difficulty
+of the increaces propotional to the number of blocks avoided.
+
+## Game Help
+
+1. Hit any key to start the game. Use the <- and -> arrows to move the car horizontally.
+1. Do not hit the sides as that ends the game.
+1. You can also move the car forward (Not Suggested!) use the up and down arrow.
+
+## Running the Game
+
+1. git clone this repository
+1. Install Python2 if not already installed
+1. install the PyGame library
+1. double click on the file named AvoidTheBolcks
+
+These instructions will get you a copy of the project up and running on your local machine. 
+
+## Built With
+
+* [Python](https://www.python.org/download/releases/2.7.2/)
+* [PyGame](https://www.pygame.org/download.shtml)
